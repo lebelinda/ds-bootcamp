e1fc7db41baa059524df95888c147629230bfcd6
Belinda Le <belindale@LMBELINDAL05068-123616.local>
2019-05-31 13:51:09 -0700

    404 Error Test. Basic linear model added. Test linear model

--- tests/test_belindal_tests.py
+++ tests/test_belindal_tests.py
@@ -1,10 +1,21 @@
 
 
 def test_get_meta(client):
-    res = client.get("/")
+    res = client.get("/belinda")
     assert res.status_code == 200
 
 
 def test_bye(client):
-    res = client.get("/bye")
+    res = client.get("/belinda_bye")
     assert res.status_code == 200
+
+
+def test_belinda_model(client):
+    res = client.get("/belinda_model/submodel_1/10")
+    assert res.status_code == 200
+    assert res.json['score'] == 10
+
+
+def test_belinda_model_404(client):
+    res = client.get("/belinda_model/submodel_3/10")
+    assert res.status_code == 404
