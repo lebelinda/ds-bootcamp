d0d7a12cde1238f3a62f32e945c1f0b11fa36857
Belinda Le <belindale@LMBELINDAL05068-123616.local>
2019-06-04 14:09:19 -0700

    Add Hypothesis test

--- ds_bootcamp/flask_app.py
+++ ds_bootcamp/flask_app.py
@@ -1,16 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import yaml
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+MAX_VALUE = 10000
+MIN_VALUE = 0
 
 
 def create_app():
     app = Flask(__name__)
+    with open(os.path.join(dir_path, 'yaml', 'belinda_model.yaml')) as f:
+        belinda_model = yaml.load(f, Loader=yaml.SafeLoader)
 
-    @app.route("/")
+    @app.route("/belinda")
     def hello_world():
         return "Hello World", 200
 
-    @app.route("/bye")
+    @app.route("/belinda_model/<submodel>/<x_1>")
+    def belinda_yaml_model(submodel, x_1):
+        if int(x_1) > MAX_VALUE:
+            return f"Value {x_1} > {MAX_VALUE}", 400
+        elif int(x_1) < MIN_VALUE:
+            return f"Value {x_1} < {MIN_VALUE}", 400
+        try:
+            model = belinda_model[submodel]
+        except KeyError:
+            return 'Model not found', 404
+        dict_ = {'score': model['b_0'] +
+                 model['b_1'] * int(x_1)}
+        return jsonify(dict_)
+
+    @app.route("/belinda_bye")
     def bye_world():
-        raise Exception("BLAH")
         return "Bye World", 200
 
     return app
--- tests/test_belindal_tests.py
+++ tests/test_belindal_tests.py
@@ -1,3 +1,5 @@
+from hypothesis import strategies as st, given, reproduce_failure
+from ds_bootcamp.flask_app import MIN_VALUE, MAX_VALUE
 
 
 def test_get_meta(client):
@@ -13,7 +15,24 @@ def test_bye(client):
 def test_belinda_model(client):
     res = client.get("/belinda_model/submodel_1/10")
     assert res.status_code == 200
-    assert res.json['score'] == 10
+    assert res.json['score'] == 25
+
+
+@given(
+    x=st.integers(min_value=MIN_VALUE, max_value=MAX_VALUE)
+)
+def test_belinda_model_hypothesis(client, x):
+    res = client.get(f"/belinda_model/submodel_1/{x}")
+    assert res.status_code == 200
+    assert res.json['score'] > x
+
+
+@given(
+    x=st.integers(max_value=MIN_VALUE-1)
+)
+def test_belinda_model_hypothesis_bad_value(client, x):
+    res = client.get(f"/belinda_model/submodel_1/{x}")
+    assert res.status_code == 400
 
 
 def test_belinda_model_404(client):
